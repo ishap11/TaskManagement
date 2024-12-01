4ea7126e97b280f640b71326b22b3ef5acfecb43
Isha <ishaprajapati112004@gmail.com>
2024-12-02 00:10:21 +0530

    done-done

--- TaskManagement-UI/TaskManagement/src/app/modules/project/project/component/all-project/all-project.component.ts
+++ TaskManagement-UI/TaskManagement/src/app/modules/project/project/component/all-project/all-project.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AllProjectService } from '../../../../../services/all-project.service';
+import { Project } from '../../../../../core/model/project';
 
 @Component({
   selector: 'TaskManagement-all-project',
@@ -6,10 +8,24 @@ import { Component } from '@angular/core';
   styleUrl: './all-project.component.scss'
 })
 export class AllProjectComponent {
-  project = {
-    name: 'TaskMaster App',
-    tasks: 24,
-    members: 5,
-    progress: 70,
-  };
+  projects: Project[] = [];
+  filteredProjects: Project[] = [];
+
+  constructor(private allproject : AllProjectService) {}
+  
+  ngOnInit(): void {
+    this.projects = this.allproject.getProjects();
+    this.filteredProjects = [...this.projects];
+  }
+
+  filterProjects(status: string): void {
+    if (status === 'active') {
+      this.filteredProjects = this.projects.filter((p) => p.progress > 0 && p.progress < 100);
+    } else if (status === 'completed') {
+      this.filteredProjects = this.projects.filter((p) => p.progress === 100);
+    } else {
+      this.filteredProjects = [...this.projects];
+    }
+  }
+
 }
