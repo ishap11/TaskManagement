f9e7de11dbe3e03f36615bc4e47fcd70b2713388
Isha <ishaprajapati112004@gmail.com>
2024-12-08 01:59:52 +0530

    all-project-done-final

--- TaskManagement-UI/TaskManagement/src/app/modules/project/project/component/all-project/all-project.component.ts
+++ TaskManagement-UI/TaskManagement/src/app/modules/project/project/component/all-project/all-project.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AllProjectService } from '../../../../../services/all-project.service';
 import { Project } from '../../../../../core/model/project';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'TaskManagement-all-project',
@@ -8,14 +9,23 @@ import { Project } from '../../../../../core/model/project';
   styleUrl: './all-project.component.scss'
 })
 export class AllProjectComponent {
-  projects: Project[] = [];
+  // projects: Project[] = [];
+  projects !: Project[]; 
+  searchText: string = '';
   filteredProjects: Project[] = [];
+  isPopupOpen = false;
+  newProject: Project = { id: 0, name: '', tasks: 0, members: 0, progress: 0 };
+  lastProjectId: number = 0; // Track the last used project ID
 
-  constructor(private allproject : AllProjectService) {}
+  constructor(private allprojectservice : AllProjectService , private router : Router) {}
   
   ngOnInit(): void {
-    this.projects = this.allproject.getProjects();
+    this.projects = this.allprojectservice.getProjects();
     this.filteredProjects = [...this.projects];
+
+    if (this.projects.length > 0) {
+      this.lastProjectId = Math.max(...this.projects.map(project => project.id)); // Get the highest ID
+    }
   }
 
   filterProjects(status: string): void {
@@ -26,6 +36,47 @@ export class AllProjectComponent {
     } else {
       this.filteredProjects = [...this.projects];
     }
+    this.applySearchFilter();
+  }
+  
+  applySearchFilter(): void {
+    if (this.searchText) {
+      this.filteredProjects = this.filteredProjects.filter((project) =>
+        project.name.toLowerCase().includes(this.searchText.toLowerCase())
+      );
+    }
+  }
+  // Open the Add Task popup
+  openTaskPopup() {
+    this.newProject.id = this.lastProjectId + 1;
+    this.isPopupOpen = true;
+  }
+
+  // Close the Add Task popup
+  closePopup() {
+    this.isPopupOpen = false;
+  }
+
+  // // Add new project
+  addProject() {
+    if (this.newProject.name && this.newProject.tasks && this.newProject.members !== undefined) {
+      // Add the new project to the projects list
+      this.projects.push({ ...this.newProject });
+      this.filteredProjects = [...this.projects];
+      
+      // Update the last project ID
+      this.lastProjectId = this.newProject.id;
+
+      // Reset the new project form
+      this.newProject = { id: 0, name: '', tasks: 0, members: 0, progress: 0 };
+      
+      // Close the popup
+      this.closePopup();
+    }
+  }
+  
+  viewDetails() {
+    this.router.navigate(['/project/view-project-details'])
   }
 
 }
---
+++ TaskManagement-UI/TaskManagement/src/app/modules/project/project/component/view-project-details/view-project-details.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'TaskManagement-view-project-details',
+  templateUrl: './view-project-details.component.html',
+  styleUrl: './view-project-details.component.scss'
+})
+export class ViewProjectDetailsComponent {
+
+}
